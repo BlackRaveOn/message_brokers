b81ad1dd61d580f73ebc541c2e8d6fba7d5e33cf
Denis Vedenkiy <vdenis2000@gmail.com>
2023-12-05 14:27:07 +0300

    add some prints

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -23,7 +23,10 @@ def consume_messages(group_id: str):
     retry_producer = Producer(producer_conf)
     error_producer = Producer(producer_conf)
 
+    print("---Waiting for messages. To exit press CTRL+C---")
+
     while True:
+
         msg = consumer.poll(1.0)
 
         if msg is None:
@@ -34,16 +37,18 @@ def consume_messages(group_id: str):
 
         decoded_msg = msg.value().decode("utf-8")
 
-        print("Received message: {}".format(msg.value().decode("utf-8")))
+        print(f"Received message: {decoded_msg}")
 
         if decoded_msg == "main":
             print(f"Received main message: {decoded_msg}")
         elif decoded_msg == "retry":
             retry_producer.produce(RETRY_TOPIC, value=decoded_msg)
             retry_producer.flush()
+            print(f"Received retry message: {decoded_msg}. Send it to retry topic")
         elif decoded_msg == "error":
             error_producer.produce(ERROR_TOPIC, value=decoded_msg)
             error_producer.flush()
+            print(f"Received retry message: {decoded_msg}. Send it to error topic")
 
 
 if __name__ == "__main__":

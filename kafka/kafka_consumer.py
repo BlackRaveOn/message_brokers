de4c68bf7eb1a5991690f68c7355bc5f868f14a6
Denis Vedenkiy <vdenis2000@gmail.com>
2023-12-05 14:41:53 +0300

    add correct logic for kafka messages

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,6 +1,8 @@
 from confluent_kafka import Consumer, Producer
 from kafka_config import KAFKA_SERVER, TOPIC, RETRY_TOPIC, ERROR_TOPIC
 import argparse
+import json
+from bson import json_util
 
 
 def consume_messages(group_id: str):
@@ -35,20 +37,32 @@ def consume_messages(group_id: str):
             print("Consumer error: {}".format(msg.error()))
             continue
 
-        decoded_msg = msg.value().decode("utf-8")
+        decoded_msg = json.loads(msg.value().decode("utf-8"))
 
         print(f"Received message: {decoded_msg}")
 
-        if decoded_msg == "main":
+        if decoded_msg["type"] == "main":
             print(f"Received main message: {decoded_msg}")
-        elif decoded_msg == "retry":
-            retry_producer.produce(RETRY_TOPIC, value=decoded_msg)
+        elif decoded_msg["type"] == "retry":
+            retry_producer.produce(
+                RETRY_TOPIC,
+                value=json.dumps(decoded_msg, default=json_util.default).encode(
+                    "utf-8"
+                ),
+            )
             retry_producer.flush()
             print(f"Received retry message: {decoded_msg}. Send it to retry topic")
-        elif decoded_msg == "error":
-            error_producer.produce(ERROR_TOPIC, value=decoded_msg)
+        elif decoded_msg["type"] == "error":
+            error_producer.produce(
+                ERROR_TOPIC,
+                value=json.dumps(decoded_msg, default=json_util.default).encode(
+                    "utf-8"
+                ),
+            )
             error_producer.flush()
             print(f"Received retry message: {decoded_msg}. Send it to error topic")
+        else:
+            print(f"Unknown message type!!! Message: {decoded_msg} was ignored")
 
 
 if __name__ == "__main__":
--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -1,22 +1,32 @@
 from confluent_kafka import Producer
 import argparse
+import json
+from bson import json_util
+
 
 from kafka_config import KAFKA_SERVER, TOPIC
 
 
-def produce_message(message: str):
+def produce_message(message: str, message_type: str):
     producer_conf = {
         "bootstrap.servers": KAFKA_SERVER,
     }
 
     producer = Producer(producer_conf)
 
-    producer.produce(TOPIC, message)
+    data = {"type": message_type, "message": message}
+
+    producer.produce(
+        TOPIC, value=json.dumps(data, default=json_util.default).encode("utf-8")
+    )
     producer.flush()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--msg", help="string to produce in Kafka", required=False)
+    parser.add_argument("--msg", help="message to produce in Kafka", required=True)
+    parser.add_argument(
+        "--type", help="type of message to produce in Kafka", required=True
+    )
     args = parser.parse_args()
-    produce_message(message=args.msg)
+    produce_message(message=args.msg, message_type=args.type)
